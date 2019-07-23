c17909e831af7d378d16be840930f4de4eecfe56
Punith K <kpunith8@gmail.com>
2019-07-23 09:01:35 +0530

    Advanced JS and funational programming samples

--- functional/ramda-sample.js
+++ functional/ramda-sample.js
@@ -3,7 +3,7 @@ const R = require("ramda");
 const arr1 = [2, 5, 8, 19, 32];
 
 console.log("Actual array", arr1);
-// Ramba accepts source as second paramente, action to be performed as first paramter
+// Ramba accepts source as second parameter, action to be performed as first paramter.
 console.log("map():", R.map(x => x + 2, arr1));
 
 const isEven = x => x % 2 === 0;
@@ -18,21 +18,21 @@ console.log(
   R.find(isOdd, arr1)
 );
 
-// `both` takes two other functions and returns a new function that returns `true`
-// if both functions return a truthy value when applied to the arguments and `false` otherwise.
+// `both()` takes two other functions and returns a new function that returns `true`
+// if `both()` functions return a truthy value when applied to the arguments and `false` otherwise.
 
-// `either` takes two other functions and returns a new function that returns `true`
-// if either function returns a truthy value when applied to the arguments and `false` otherwise.
+// `either()` takes two other functions and returns a new function that returns `true`
+// if `either()` function returns a truthy value when applied to the arguments and `false` otherwise.
 
 // R.both(isEven, isGreaterThan5)
 // R.either(isEven, isGreaterThan5)
 
 console.log("both():", R.filter(R.both(isEven, isGreatherThan5), arr1));
 
-// Ramda also provides `allPass` and `anyPass` that take an array of any number of functions.
-// As their names suggest, `allPass` works like `both`, and `anyPass` works like `either`.
+// Ramda also provides `allPass()` and `anyPass()` that take an array of any number of functions.
+// As their names suggest, `allPass()` works like `both()`, and `anyPass()` works like `either`.
 
-// Ramda provides the `pipe` function, which takes a list of one or more functions and returns a new function.
+// Ramda provides the `pipe()` function, which takes a list of one or more functions and returns a new function.
 // functions after the first must only take a single argument.
 
 const multiply = (a, b) => a * b;
@@ -60,7 +60,8 @@ console.log("Using pipe():", operatePipe(3, 4));
 // Composing
 // const operate = (x, y) => square(addOne(multiply(x, y)))
 
-// 'compose' works exactly the same way as pipe, except that it applies the functions in `right-to-left` order instead of left-to-right
+// 'compose()' works exactly the same way as `pipe()`, 
+// except that it applies the functions in `right-to-left` order instead of left-to-right
 const operateCompose = R.compose(
   square,
   addOne,
@@ -68,3 +69,76 @@ const operateCompose = R.compose(
 );
 
 console.log("Using compose():", operateCompose(4, 5));
+
+// Partial applications
+const books = [
+  {
+    title: "book-1",
+    year: 2000
+  },
+  {
+    title: "book-2",
+    year: 2001
+  },
+  {
+    title: "book-3",
+    year: 2000
+  },
+  {
+    title: "book-4",
+    year: 2001
+  }
+];
+
+/* Without using partial */
+// const publishedInYear = (book, year) => book.year === year;
+
+// const titlesForYear = (books, year) => {
+//   const selected = R.filter(book => publishedInYear(book, year), books);
+
+//   return R.map(book => book.title, selected);
+// };
+
+// This can also be written as higher order function to pass one param instead of multiple to filter
+// here `filter()` wants a function accepting a param, in this case book and checks for the both condition
+/*
+const publishedInYear = year => book => book.year === year;
+
+const titlesForYear = (books, year) => {
+  const selected = R.filter(publishedInYear(year), books)
+ 
+  return R.map(book => book.title, selected)
+}
+*/
+
+/*
+`partial()` and `partialRight()`
+
+These two functions let us call any function with fewer arguments than it needs. 
+They both return a new function that takes the missing arguments and then calls the 
+original function once all of the arguments have been supplied.
+
+The difference between `partial()` and `partialRight()` is whether the arguments 
+we supply are the left-most or right-most arguments needed by the original function.
+*/
+
+// Above example can be written using `partialRight()` as follows
+const publishedInYear = (book, year) => book.year === year;
+ 
+const titlesForYear = (books, year) => {
+  // Would have used partial if the param to publichedInYear() are reversed
+  const selected = R.filter(R.partialRight(publishedInYear, [year]), books);
+ 
+  return R.map(book => book.title, selected);
+}
+
+// Note that the arguments we supply to partial and partialRight must always be in an array, even if there’s only one of them
+
+console.log("Books filtered for year 2001:", titlesForYear(books, 2000));
+
+// CURRYING
+
+
+
+
+
