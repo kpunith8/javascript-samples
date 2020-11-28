8d6efa701ed00c6b0e0e692c39b8e7a58d8a5d61
Punith K <kpunith8@gmail.com>
2020-11-28 09:03:57 +0530

    Added FP examples

--- functional/mastering-fp.js
+++ functional/mastering-fp.js
@@ -1,4 +1,5 @@
 // Mastering JS Functional Programming - Kereki Fedrico
+const { performance } = require('perf_hooks');
 
 // Classes as first class objects
 const makeHelloClass = (greeting) =>
@@ -33,7 +34,7 @@ const once = (fn) => {
 };
 
 const showMessage = (msg) => console.log(`Hello ${msg}`);
-const greet = msg => console.log(`Greet ${msg}`)
+const greet = (msg) => console.log(`Greet ${msg}`);
 
 const showOnce = once(showMessage);
 showOnce('World once');
@@ -62,18 +63,134 @@ const alternator = (f, g) => {
       done = true;
       f(...args);
     } else {
-      done = false
+      done = false;
       g(...args);
     }
   };
 };
 
 const showMessage1 = () => console.log(`1`);
-const greet1 = () => console.log(`2`)
-const alternativeFn = alternator(showMessage1, greet1)
-alternativeFn()
-alternativeFn()
-alternativeFn()
-alternativeFn()
+const greet1 = () => console.log(`2`);
+const alternativeFn = alternator(showMessage1, greet1);
+alternativeFn();
+alternativeFn();
+alternativeFn();
+alternativeFn();
+
+// memoization with fibonacci series
+let cache = [];
+const fib = (n) => {
+  console.time(`fib(${n})`);
+  if (cache[n] == undefined) {
+    if (n == 0) {
+      cache[0] = 0;
+    } else if (n == 1) {
+      cache[1] = 1;
+    } else {
+      cache[n] = fib(n - 2) + fib(n - 1);
+    }
+  }
+  console.timeEnd(`fib(${n})`);
+
+  return cache[n];
+};
+
+// This function doesn't do any unnecessary or repeated computations.
+const fibNormal = (n, a = 0, b = 1) =>
+  n === 0 ? a : fibNormal(n - 1, b, a + b);
+
+console.log('fibNormal', fibNormal(11));
+
+console.log(fib(4));
+console.log(fib(6));
+console.log(fib(5));
+
+const shuffle = (arr) => {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let r = Math.floor(Math.random() * (len - i));
+    [arr[i], arr[i + r]] = [arr[i + r], arr[i]];
+  }
+  return arr;
+};
+
+var arr1 = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+console.log('Shuffled array', shuffle(arr1));
+
+// Higher Order Functions
+// Passing console.log as logger to the withLog to make it
+// side effects free
+const withLog = (fn, logger = console.log) => (...args) => {
+  try {
+    logger(`Entering ${fn.name}: ${args}`);
+    return fn(...args);
+  } catch (error) {
+    logger(`Exiting ${fn.name}: threw ${error}`);
+    throw error;
+  }
+};
+
+function subtract(a, b) {
+  b = changeSign(b);
+  return a + b;
+}
+
+function changeSign(a) {
+  return -a;
+}
+
+const subtractWithLog = withLog(subtract);
+changeSign = withLog(changeSign);
+subtractWithLog(7, 5);
+
+const getPerf = () => performance.now();
+const timeLogger = (msg, fnName, start, end) =>
+  console.log(`${fnName} - ${msg} took ${end - start} ms`);
+
+const withTiming = (fn, getTime = getPerf, logger = timeLogger) => (
+  ...args
+) => {
+  const start = getTime();
+
+  try {
+    logger('Normal exit', fn.name, start, getTime());
+    return fn(...args);
+  } catch (error) {
+    logger('Exception thrown', fn.name, start, getTime());
+    throw error;
+  }
+};
+
+const withTimeSubtract = withTiming(subtract);
+
+withTimeSubtract(10, 20);
+
+function fib2(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fib2(n - 2) + fib2(n - 1);
+  }
+}
+
+// memoization
+const memoize = (fn) => {
+  let cache = {};
+  return (x) => (x in cache ? cache[x] : (cache[x] = fn(x)));
+};
+
+fib2 = memoize(fib2)
+const testFib = (n) => fib2(n);
+withTiming(testFib)(40);
+withTiming(testFib)(30);
+withTiming(testFib)(40);
+
+const testMemoFib = memoize(testFib);
+withTiming(testMemoFib)(40);
+withTiming(testMemoFib)(20);
+withTiming(testMemoFib)(40);
+withTiming(testMemoFib)(20);
 
 module.exports = { once, onceAndAfter };
